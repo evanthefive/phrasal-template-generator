9e6fa2179793342be0dbd01d888422efe220e3d0
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-03-04 12:21:34 -0500

    Created functions and story positioning

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ var creature = prompt('A creature (dragon, moth, etc.)');
 var adj = prompt('An adjective (beautiful, super, etc.)');
 var past = prompt('A past tense verb (ran, burped, etc.)');
 var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning');
+
+var writeAstoryA = function () {
+  document.write('After snacking on ' + adj + ' treats, the ' + colour + ' bellied ' + creature + ' ' + past + ' for hours ');
+};
+
+var writeAstoryB = function () {
+  document.write('Ameilia ' + past + ' through the ' + adj + ' ' + colour + ' nebula escaping the space ' + creature + '.');
+};
+
+var writeAstoryC = function () {
+  document.write('Jackson chose his ' + adj + ' ' + colour + ' ' + creature + ' card and ' + past + ' it to the table knowing he won.');
+};
+
+if (story === 'a') {
+  writeAstoryA(colour, creature, adj, past);
+}
+
+if (story === 'b') {
+  writeAstoryB(colour, creature, adj, past);
+}
+
+if (story === 'c') {
+  writeAstoryC(colour, creature, adj, past);
+}
